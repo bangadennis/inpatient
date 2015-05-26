73829661aebda9f570e311fbf3ac1236788db22c
banga <dennisbanga@gmail.com>
2015-05-26 22:17:10 +0300

    wardservice interface its implementation

--- api/src/main/java/org/openmrs/module/inpatient/api/WardService.java
+++ api/src/main/java/org/openmrs/module/inpatient/api/WardService.java
@@ -1,7 +1,26 @@
 package org.openmrs.module.inpatient.api;
 
+import org.openmrs.api.OpenmrsService;
+import org.openmrs.module.inpatient.Ward;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Created by banga on 5/26/15.
  */
-public interface WardService {
+@Transactional
+public interface WardService extends OpenmrsService {
+
+    //list all wards
+    @Transactional(readOnly = true)
+    List<Ward> getAllWards();
+    //gets a ward
+    @Transactional(readOnly = true)
+    Ward getWard(Integer wardId);
+    //Saves a ward
+    Ward saveWard(Ward ward);
+    //deletes a ward
+    void purgeWard(Ward ward);
+
 }
--- api/src/main/java/org/openmrs/module/inpatient/api/impl/WardServiceImpl.java
+++ api/src/main/java/org/openmrs/module/inpatient/api/impl/WardServiceImpl.java
@@ -1,7 +1,52 @@
 package org.openmrs.module.inpatient.api.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.inpatient.Ward;
+import org.openmrs.module.inpatient.api.WardService;
+import org.openmrs.module.inpatient.api.db.WardDAO;
+
+import java.util.List;
+
 /**
  * Created by banga on 5/26/15.
  */
-public class WardServiceImpl {
+public class WardServiceImpl extends BaseOpenmrsService implements WardService {
+
+    protected final Log log = LogFactory.getLog(this.getClass());
+
+    private WardDAO dao;
+
+    public void setDao(WardDAO dao) {
+        this.dao = dao;
+    }
+    /**
+     * @return the dao
+     */
+    public WardDAO getDao() {
+        return dao;
+    }
+
+
+    @Override
+    public List<Ward> getAllWards() {
+        return dao.getAllWards();
+    }
+
+    @Override
+    public Ward getWard(Integer wardId) {
+        return dao.getWard(wardId);
+    }
+
+    @Override
+    public Ward saveWard(Ward ward) {
+        return dao.saveWard(ward);
+    }
+
+    @Override
+    public void purgeWard(Ward ward) {
+        dao.purgeWard(ward);
+
+    }
 }
