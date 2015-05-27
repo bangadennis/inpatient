febfb15db1b66a94075d1d8574afa67f88f71007
banga <dennisbanga@gmail.com>
2015-05-27 14:58:46 +0300

    added methods for deleting a ward and editing a ward

--- omod/src/main/java/org/openmrs/module/inpatient/web/controller/InpatientManageController.java
+++ omod/src/main/java/org/openmrs/module/inpatient/web/controller/InpatientManageController.java
@@ -111,17 +111,101 @@ public class  InpatientManageController {
 			try {
 				wardService.saveWard(ward);
 				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Added Successfully");
-				return "redirect:addward.form";
+				return "redirect:listwards.form";
 
 			}
 				catch (Exception ex) {
 
-				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Addition was unsuccessfully");
+				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Addition failed");
 			}
 
 		}
 
 		return "redirect:addward.form";
 	}
+
+
+	//method for deleting  ward
+	@RequestMapping(value = "/module/inpatient/deleteWard.form", method=RequestMethod.GET)
+	public String  deleteWard(ModelMap model, WebRequest webRequest, HttpSession httpSession,
+						  @RequestParam(value = "id", required = true) Integer wardId) {
+		try {
+			WardService wardService=Context.getService(WardService.class);
+			Ward ward=wardService.getWard(wardId);
+			wardService.purgeWard(ward);
+			httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Deleted Successfully");
+			return "redirect:listwards.form";
+		}
+		catch (Exception ex)
+		{
+			httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Deletion failed");
+			return "redirect:listwards.form";
+
+		}
+
+		//return "redirect:manage.form";
+	}
+
+	//method to display Edit form for Ward
+	@RequestMapping(value = "/module/inpatient/editWard.form", method=RequestMethod.GET)
+	public void editWardForm(ModelMap model, WebRequest webRequest, HttpSession httpSession,
+						 @RequestParam(value = "id", required = true) Integer wardId) {
+		try {
+			WardService wardService=Context.getService(WardService.class);
+			Ward ward=wardService.getWard(wardId);
+			model.addAttribute("ward", ward);
+
+		}
+		catch (Exception ex)
+		{
+			httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Edit form failed");
+
+		}
+
+		//return "redirect:manage.form";
+	}
+
+	//Method to save an Ward edit
+	@RequestMapping(value = "/module/inpatient/saveWardEdit.form", method=RequestMethod.POST)
+	public String saveWardEdit(WebRequest request, HttpSession httpSession, ModelMap model,
+							   @RequestParam(required = true, value = "id") Integer wardId,
+							   @ModelAttribute("ward") Ward ward, BindingResult errors)
+	{
+
+		WardService wardService = Context.getService(WardService.class);
+
+
+		if (!Context.isAuthenticated()) {
+			errors.reject("ward.auth.required");
+
+		} else
+		{
+
+			try {
+
+				Ward wardEdit=wardService.getWard(wardId);
+
+				wardEdit.setWardName(ward.getWardName());
+				wardEdit.setSpeciality(ward.getSpeciality());
+				wardEdit.setDescription(ward.getDescription());
+				wardEdit.setCapacity(ward.getCapacity());
+				wardEdit.setUuid(ward.getUuid());
+
+				wardService.saveWard(wardEdit);
+				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Edited Ward Successfully");
+				return "redirect:listwards.form";
+
+			}
+			catch (Exception ex) {
+
+				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Ward Edit failed");
+			}
+
+		}
+
+		return "redirect:listwards.form";
+	}
+
+
 
 }
